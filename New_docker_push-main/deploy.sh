f2ab0934f22dc7f6a7cc2255d49292b5fd9738ee
PRADEEP V <146934039+PRADEEP-VELLIRASAN@users.noreply.github.com>
2025-03-21 11:38:45 +0530

    Update deploy.sh

--- New_docker_push-main/deploy.sh
+++ New_docker_push-main/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set Docker image name, tag, and container name
-IMAGE_NAME="pradeepv2006/dockerproject"
+IMAGE_NAME="pradeepv2006/proj2"
 TAG="latest"
 CONTAINER_NAME="my-container"
 
